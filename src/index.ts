18da9fcd93c8ad7dec66bd68e3b22ec1c11815fb
David Codina <davidmcodina@icloud.com>
2026-08-09 15:46:13 -0600

    WIP: Sun Aug 09, 2026 at 15:46:13

--- src/index.ts
+++ src/index.ts
@@ -126,18 +126,13 @@ app.get('/api/posts', async (req: Request, res: Response) => {
       message: 'success',
       success: true
     })
-  } catch (err) {
+  } catch (_err) {
     // if (err instanceof Error) { console.log({ name: err.name, message: err.message }) }
-
-    //! Temporary:
-    const errorMessage = err instanceof Error ? err.message : 'Server error.'
-
-    console.log('err from posts:', err)
 
     return res.status(500).json({
       data: null,
-      //! message: 'Server error.',
-      message: errorMessage,
+      message: 'Server error.',
+
       success: false
     })
   }
